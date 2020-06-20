97e6908dcf88d94bd567a3dbbc7892338dbc59f0
Kun Hwi Ko <khk305@stern.nyu.edu>
2020-06-19 23:20:13 -0400

    initialize tetris game board

---
+++ Tetris/GameUI/TetrisGameUI.swift
@@ -0,0 +1,26 @@
+//
+//  TetrisGameUI.swift
+//  Tetris
+//
+//  Created by Kun Hwi Ko on 6/19/20.
+//  Copyright © 2020 Kun Hwi Ko. All rights reserved.
+//
+
+import SwiftUI
+
+struct TetrisBoard {
+    var color : Color
+}
+
+class TetrisGameUI : ObservableObject {
+    var rows : Int
+    var cols : Int
+    var board : [[TetrisBoard]]
+    
+    init(rows:Int = 23, cols:Int = 10){
+        self.rows = rows
+        self.cols = cols
+        
+        self.board = Array(repeating: Array(repeating: TetrisBoard(color: Color.black), count: cols), count: rows)
+    }
+}
