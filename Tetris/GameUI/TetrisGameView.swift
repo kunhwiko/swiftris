2ad8d9f713544f2dbbd7af1df436cff28df14498
Kun Hwi Ko <khk305@stern.nyu.edu>
2020-06-19 23:46:07 -0400

    finish viewer

--- Tetris/GameUI/TetrisGameView.swift
+++ Tetris/GameUI/TetrisGameView.swift
@@ -12,7 +12,32 @@ struct TetrisGameView: View {
     @ObservedObject var tetris = TetrisGameUI()
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { (geometry: GeometryProxy) in
+            self.draw(boundingRect: geometry.size)
+        }
+    }
+    
+    func draw(boundingRect: CGSize) -> some View {
+        let columns = self.tetris.cols
+        let rows = self.tetris.rows
+        let blockSize = min(boundingRect.width/CGFloat(columns),
+                            boundingRect.height/CGFloat(rows))
+        let colOffset = (boundingRect.width - blockSize*CGFloat(columns))/2
+        let rowOffset = (boundingRect.height - blockSize*CGFloat(rows))/2
+        let board = self.tetris.board
+        
+        return ForEach(0...columns-1, id:\.self) { (column:Int) in
+            ForEach(0...rows-1, id:\.self) { (row:Int) in
+                Path { path in
+                    let x = colOffset + blockSize*CGFloat(columns)
+                    let y = boundingRect.height - rowOffset - blockSize*CGFloat(row+1)
+                    let square = CGRect(x:x, y:y, width:blockSize, height:blockSize)
+                    path.addRect(square)
+                }
+                .fill(board[row][column].color)
+            }
+        
+        }
     }
 }
 
